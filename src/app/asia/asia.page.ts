20152d63ef226621d8d35882222bb596929e664c
Mauro Vitale <vitalemauro@outlook.it>
2018-12-03 12:08:41 +0100

    Partial sentence preview added

--- src/app/asia/asia.page.ts
+++ src/app/asia/asia.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { Bubbles } from "chat-bubble/component/Bubbles.js";
 import { ViewEncapsulation } from '@angular/core';
 
@@ -18,8 +18,12 @@ export class AsiaPage implements OnInit
 
   private isSpeechRecognizerAvailable:boolean;
   private speechRecognizerOptions;
+  private lastPartialSentence:string;
 
-  constructor(private speechRecognizer: SpeechRecognition){
+  private PARTIAL_SENTENCE_ID="show-partial";
+  private PARTIAL_SENTENCE_CONTAINER_ID="show-partial-container";
+
+  constructor(private speechRecognizer: SpeechRecognition, private ngZone:NgZone){
   }
 
   ngOnInit(){
@@ -28,7 +32,8 @@ export class AsiaPage implements OnInit
     this.vocalInput=false;
     this.lastMessageOwner="asia";
     this.isSpeechRecognizerAvailable=false;
-    //Check per l'attivazione di SpeechRecognition
+    this.lastPartialSentence=". . .";
+    //Check permessi per l'attivazione di SpeechRecognition
     this.speechRecognizer.isRecognitionAvailable().then((available:boolean)=>{
       console.log("Speech Recognition Available:"+available);
       this.speechRecognizer.hasPermission().then((hasPermission:boolean)=>{
@@ -37,12 +42,12 @@ export class AsiaPage implements OnInit
           this.speechRecognizer.requestPermission().then(()=>{
             console.log("Voice Recording Permission Granted");
             this.isSpeechRecognizerAvailable=true;
-            this.speechRecognizerOptions={language:'it-IT',showPopup:false};
+            this.speechRecognizerOptions={language:'it-IT',showPopup:false,showPartial:true};
           },
             ()=>console.log("Voice Recording Permission Denied"));
         } else if(available&&hasPermission){
           this.isSpeechRecognizerAvailable=true;
-          this.speechRecognizerOptions={language:'it-IT',showPopup:false};
+          this.speechRecognizerOptions={language:'it-IT',showPopup:false,showPartial:true};
         }
       });
     });
@@ -50,16 +55,44 @@ export class AsiaPage implements OnInit
 
   switchToVocal():void{
     this.vocalInput=true;
+    this.createSpeech2TextBubble();
     if(this.isSpeechRecognizerAvailable){
       console.log("Start Speech Detection....");
       this.speechRecognizer.startListening(this.speechRecognizerOptions).subscribe(
-        (matches: string[]) => console.log(matches),
-        (onerror) => console.log('error:', onerror));
+        (matches: string[]) => {
+          //Se la frase parziale ottenuta è uguale alla precedente
+          //l'ascolto è terminato
+          if(matches[0]===this.lastPartialSentence){
+            //Necessario per entrare in una zona Angular
+            //ed aggiornare il front-end da un thread come subscribe
+            this.ngZone.run(()=>{
+              console.log("NgZone running...");
+              //Rimozione id dalla bubble di speech
+              document.getElementById(this.PARTIAL_SENTENCE_CONTAINER_ID).removeAttribute("id");
+              document.getElementById(this.PARTIAL_SENTENCE_ID).removeAttribute("id");
+              //Passaggio alla modalità testuale ad ascolto finito
+              this.switchToTextual();
+            })
+            console.log("Listening Ended");
+          }else{
+            //Modifica del testo parziale nella speech bubble
+            var speechBubble=document.getElementById(this.PARTIAL_SENTENCE_ID);
+            speechBubble.innerText=matches[0];
+            this.lastPartialSentence=matches[0];
+          }
+        },
+        (onerror) => console.log('Error:', onerror));
     }
   }
 
   switchToTextual():void{
     this.vocalInput=false;
+    this.lastPartialSentence=". . .";
+    //Se l'ascolto è stato interrotto viene rimossa
+    //la speech bubble temporanea
+    var speechBubbleContainer=document.getElementById(this.PARTIAL_SENTENCE_CONTAINER_ID);
+    if(speechBubbleContainer!=null)
+      speechBubbleContainer.remove();
     if(this.isSpeechRecognizerAvailable){
       this.speechRecognizer.stopListening();
       console.log("Speech Detection Stopped");
@@ -69,14 +102,16 @@ export class AsiaPage implements OnInit
   sendMessage(){
     var send_btn=document.getElementById("send-btn-img");
     //Aggiunta dell'animazione al bottone
-    send_btn.classList.add("animated");
-    send_btn.classList.add("pulse");
+    if(send_btn!=null){
+      send_btn.classList.add("animated");
+      send_btn.classList.add("pulse");
+    }
 
     this.textMessage=this.textMessage.trim();
     if(this.textMessage!=''){
       var div_chat=document.getElementById("chat");
       var bubble_wrap= div_chat.firstChild;
-      var messageElement= this.createMessageElement(this.textMessage);
+      var messageElement= this.createMessageElement(this.textMessage,false);
       setTimeout(function(){
         bubble_wrap.appendChild(messageElement);
         //scroll
@@ -89,15 +124,28 @@ export class AsiaPage implements OnInit
 
     //Thread per la rimozione dell'animazione quando il messaggio
     //è stato inviato
-    setTimeout(function(){
-      send_btn.classList.remove("animated");
-      send_btn.classList.remove("pulse");
-    },1000);
+    if(send_btn!=null)
+      setTimeout(function(){
+        send_btn.classList.remove("animated");
+        send_btn.classList.remove("pulse");
+      },1000);
   }
 
-  createMessageElement(text:string){
+  createSpeech2TextBubble(){
+    var div_chat=document.getElementById("chat");
+    var bubble_wrap= div_chat.firstChild;
+    var messageElement=this.createMessageElement(this.lastPartialSentence,true);
+    setTimeout(function(){
+      bubble_wrap.appendChild(messageElement);
+      div_chat.scrollTop = div_chat.scrollHeight;
+    },100);
+    this.lastMessageOwner='user';
+    this.textMessage="";
+  }
+
+  createMessageElement(text:string,speech2Text:boolean){
     var msgContainer=document.createElement("div");
-    console.log(this.lastMessageOwner)
+    //Check del proprieterio dell'ultimo messaggio
     var firstReplyClass=this.lastMessageOwner==='asia'?"first-reply":"";
     msgContainer.setAttribute("class","message-container  reply-container "+firstReplyClass+" animated fadeIn");
     msgContainer.setAttribute("style","margin-right: 17px;")
@@ -107,6 +155,11 @@ export class AsiaPage implements OnInit
     bubble_content.setAttribute("class","bubble-content");
     var text_container=document.createElement("span");
     text_container.setAttribute("class","bubble-button bubble-pick");
+    if(speech2Text){
+      //ids per il controllo della speech bubble
+      msgContainer.setAttribute("id",this.PARTIAL_SENTENCE_CONTAINER_ID);
+      text_container.setAttribute("id",this.PARTIAL_SENTENCE_ID);
+    }
     text_container.innerText=text;
 
     bubble_content.appendChild(text_container);
