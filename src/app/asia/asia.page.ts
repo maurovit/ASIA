facbf5fd04fce83a220955d2b194bec1997813b8
Vincenzo <vincenzo.gallicchio.web@gmail.com>
2019-02-15 18:05:58 +0100

    Notifiche attive solo quando l'applicazione è in pausa

--- src/app/asia/asia.page.ts
+++ src/app/asia/asia.page.ts
@@ -65,23 +65,27 @@ export class AsiaPage implements OnInit
       });   
       //Si inizializza il warning level   
       this.warningLevel = 0;
-      //Si calcola l'indice della frase di Asia
+      
+
+      this.platform.pause.subscribe(()=>{
+        //Si calcola l'indice della frase di Asia
       let random_number = Math.random() * 2;
       var index = Math.floor(random_number);
+      this.asiaFirstString = this.AsiaEntranceSentences[index];
+        this.localNotifications.schedule({
+          title: 'Asia dice',
+          text: this.AsiaEntranceSentences[index],
+          trigger: {at: new Date(new Date().getTime() + 5000)},
+          //trigger: { every: { minute: 34 }},
+          led: 'FFFFFF',
+          sound: this.platform.is('android')? 'file://sound.mp3': 'file://beep.caf',
+          icon : 'C:\\Users\\hp\\Desktop\\Logo finale\\logo-finale.png' //url
+       });
+      });
 
-      this.localNotifications.schedule({
-        text: this.AsiaEntranceSentences[index],
-        trigger: {at: new Date(new Date().getTime() + 7000)},
-        led: 'FF0000',
-        sound: this.platform.is('android')? 'file://sound.mp3': 'file://beep.caf',
-        icon : 'assets/img/asia_avatar.png' //url
-     });
-
-     this.localNotifications.on('click').subscribe(()=>{
-       //Non funziona il porco binding
-       this.asiaFirstString = this.AsiaEntranceSentences[index];
-       this.presentToast(this.asiaFirstString);
-     })
+      this.platform.resume.subscribe(()=>{
+        this.localNotifications.cancel;
+      });
 
   }
 
@@ -448,6 +452,7 @@ async uploadImageData(entry) {
         
         if(value < 0.4){
           this.warningLevel++;
+          this.presentToast("Debug: warning level aumentato");
         }
         if(this.warningLevel == 8){
           //Contatta operatore
